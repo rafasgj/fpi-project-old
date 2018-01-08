0c763ca94d036d3ecea19aafdd92f4b1ef16f936
Rafael Guterres Jeffman <rafasgj@gmail.com>
2018-01-08 18:42:28 -0200

    Fixes, for now, script to run all tests features.
     Issue Number: N/A

--- fpi/tests/behave.sh
+++ fpi/tests/behave.sh
@@ -2,5 +2,7 @@
 
 for feature in `ls -1d *.features | sort`
 do
-    behave-3 "${feature}" || exit $?
+    pushd "${feature}" >/dev/null 2>&1
+    PYTHONPATH="$PYTHONPATH:../.." behave-3 || exit $?
+    popd >/dev/null 2>&1
 done
