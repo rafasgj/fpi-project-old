08131e294ccb28fe437a032ead8399a0334f513d
Rafael Guterres Jeffman <rafasgj@gmail.com>
2018-01-08 23:50:21 -0200

    Restructured Behave files to work with separate feature directories. There is an issue with Behave (see Behave on Github, issue 169) that prevent it from using separate 'steps' directories. To overcome that limitation, this project will use separate features directories (one for each sub-project) and one directory for steps, with steps named as if they were 'modules'.
    Issue Number: N/A

--- fpi/tests/behave.sh
+++ fpi/tests/behave.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-for feature in `ls -1d *.features | sort`
-do
-    pushd "${feature}" >/dev/null 2>&1
-    PYTHONPATH="$PYTHONPATH:../.." behave-3 || exit $?
-    popd >/dev/null 2>&1
-done
+PYTHONPATH="$PYTHONPATH:.." behave-3
