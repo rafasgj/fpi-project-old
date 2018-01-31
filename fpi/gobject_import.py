9feaf21c65fa3f8376a1658e00558946af7a63ca
Rafael Guterres Jeffman <rafasgj@gmail.com>
2018-01-31 14:23:03 -0200

    Updated documentation for Flake8.

--- fpi/gobject_import.py
+++ fpi/gobject_import.py
@@ -1,4 +1,8 @@
-"""Setup all versions required for Gobject Introspection."""
+"""Setup all versions required for Gobject Introspection.
+
+When importing this file you should add '# noqa: F401' in the import
+line, so that flake8 does not complain that the module is not used.
+"""
 
 import gi
 gi.require_version("GExiv2", "0.10")
