ffe97f403a20be88932c12e5ac89740c14acc046
Rafael Guterres Jeffman <rafasgj@gmail.com>
2018-02-01 00:18:01 -0200

    Implements ingestion by addition of directories, recursively or not.
    Issue: Fixes #2

--- fpi/catalog.py
+++ fpi/catalog.py
@@ -6,9 +6,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 import datetime
+import os
 
 from base import Base
-
 import dao
 
 
@@ -43,14 +43,30 @@ class Catalog(object):
         else:
             raise Exception("Refusing to overwrite catalog.")
 
-    def ingest(self, method, filelist, session_name=None):
+    def ingest(self, method, filelist, session_name=None, options=None):
         """Ingest the files in filelist using the provided method."""
+        recurse = 'recurse' in options if options is not None else False
         if session_name is None:
             now = datetime.datetime.utcnow()
             session_name = now.strftime("%Y-%m-%dT%H:%M:%S.%f%z")
         session = self.Session()
         if method == 'add':
-            for file in filelist:
-                asset = dao.Asset(file, session_name)
-                session.add(asset)
+            for f in filelist:
+                if os.path.isfile(f):
+                    self.__ingest_file(session, session_name, f)
+                else:
+                    self.__ingest_dir(session, session_name, f, recurse)
+
         session.commit()
+
+    def __ingest_dir(self, session, session_name, dirname, recurse):
+        for entry in os.scandir(dirname):
+            if entry.is_file():
+                self.__ingest_file(session, session_name, entry.path)
+            else:
+                if recurse:
+                    self.__ingest_dir(session, session_name, entry.path, True)
+
+    def __ingest_file(self, session, session_name, filename):
+        asset = dao.Asset(filename, session_name)
+        session.add(asset)
