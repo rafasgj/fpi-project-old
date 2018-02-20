4cd293f5b5b44c8ddc20a573c795f95340dc3767
Rafael Guterres Jeffman <rafasgj@gmail.com>
2018-02-19 21:48:09 -0300

    Added 'flag' property to Images.
    Issue: #28

---
+++ fpi/tests/fpi.features/attribute.features/flag.feature
@@ -0,0 +1,23 @@
+Feature: Manage asset flags (pick/reject/none) and use them to search assets.
+
+Scenario Outline: Add flags to images.
+    Given a catalog named "test_catalog.fpicat" with some assets
+        | filename                                |
+        | data/samples/DCIM/100FPIAM/FPI_0001.JPG |
+        | data/samples/DCIM/100FPIAM/FPI_0002.JPG |
+        | data/samples/DCIM/100FPIAM/FPI_0003.JPG |
+        | data/samples/DCIM/100FPIAM/FPI_0004.JPG |
+        | data/samples/DCIM/100FPIAM/FPI_0005.JPG |
+        | data/samples/DCIM/100FPIAM/FPI_0006.JPG |
+        | data/samples/DCIM/100FPIAM/FPI_0007.JPG |
+    When setting the flag of <image> of <asset> to <flag>
+    Then no exception is raised
+        And the flag of <image> of <asset> to <flag> is set
+
+    Examples:
+    | asset                            | image | flag      |
+    | 3b1479d722fbe11df5677bb521e2575b |   1   | pick      |
+#    | 3b1479d722fbe11df5677bb521e2575b |   1   | reject    |
+#    | c8b07cb389edaaf736b2486361b5e593 |   1   | pick      |
+#    | c8b07cb389edaaf736b2486361b5e593 |   1   | reject    |
+#    | 8dde366bfc65efd9fabcc74728061740 |   1   | unflagged |
---
+++ fpi/tests/steps/attributes.py
@@ -0,0 +1,51 @@
+"""Steps to test managing image attributes."""
+
+from behave import given, when, then
+
+import catalog
+
+import dao
+
+
+def _get_image_from_catalog(catalog, asset, image):
+    img = int(image.strip()) - 1
+    ast = catalog.info('asset', asset)
+    return ast.virtual_copies[img]
+
+
+@given('a catalog named "{catalog_name}" with some assets')
+def given_catalog_with_assets(context, catalog_name):
+    """Create a catalog with some assets."""
+    try:
+        cat = catalog.Catalog(catalog_name)
+        context.catalog = cat
+        cat.create()
+        cat.ingest('add', [r['filename'] for r in context.table])
+        context.exception = None
+    except Exception as e:
+        context.exception = e
+
+
+@when('setting the flag of {image} of {asset} to {flag}')
+def when_setting_image_flag(context, asset, image, flag):
+    """Set image flag to the given value."""
+    if flag.strip().lower() == 'pick':
+        f = dao.Image.PICK
+    elif flag.strip().lower() == 'reject':
+        f = dao.Image.REJECT
+    else:
+        f = dao.Image.UNFLAG
+    img = _get_image_from_catalog(context.catalog, asset, image)
+    img.set_flag(f)
+
+
+@then('the flag of {image} of {asset} to {flag} is set')
+def then_flag_is_set_and_no_other_flag(context, image, asset, flag):
+    """Check that a specific flag is set."""
+    img = _get_image_from_catalog(context.catalog, asset, image)
+    if flag.strip().lower() == 'pick':
+        assert img.pick is True
+    elif flag.strip().lower() == 'reject':
+        assert img.reject is True
+    else:
+        assert img.unflagged is True
