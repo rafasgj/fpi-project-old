08ce5c8503557bea28351c6ecdc828e2d22bca93
Rafael Guterres Jeffman <rafasgj@gmail.com>
2018-05-14 20:29:10 -0300

    Created execptions for invalid catalog version and inexistent catalog.

--- fpi/errors.py
+++ fpi/errors.py
@@ -1,4 +1,13 @@
 """Define exceptions that can raised."""
 
+
+class InexistentCatalog(Exception):
+    """Define an error for unexistent databases."""
+
+    pass
+
+
 class UnexpectedCatalogVersion(Exception):
+    """Define an error for databases that need to be upgraded."""
+
     pass
